2379d13d2b536ee7e318c21b9da09c63b92669ce
Leigh-ann <leighann.gant@gmail.com>
2018-01-16 17:45:18 +0000

    code refactored

--- app.rb
+++ app.rb
@@ -9,15 +9,13 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name =  params[:player_2_name]
-    session[:names] = @player_1_name, @player_2_name
+    session[:names] = params[:player_1_name], params[:player_2_name]
     redirect '/play'
   end
 
   get '/play' do
     @player_1_name, @player_2_name = session[:names]
-    erb :play 
+    erb :play
   end
 
   run! if app_file == $0 # adding this allows you to run ruby app.rb within IRB
