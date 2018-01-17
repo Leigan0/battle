85e57304aea8c5699444eea7c9e447e62afd6eee
Leigh-ann <leighann.gant@gmail.com>
2018-01-17 18:46:15 +0000

    fixed player switch turns

--- spec/features/game_switch_turns_spec.rb
+++ spec/features/game_switch_turns_spec.rb
@@ -6,4 +6,20 @@ feature 'Switch turns after action' do
     click_button 'attack'
     expect(page).to have_content('Simon hit points 90')
   end
+
+  feature 'Shows player turn' do
+    scenario 'show player 1 turn' do
+      sign_in_and_play
+      expect(page).to have_content("Simon's turn")
+      expect(page).not_to have_content("Leigh-ann's turn")
+    end
+
+    scenario 'show player 2 turn' do
+      sign_in_and_play
+      click_button 'attack'
+      expect(page).to have_content("Leigh-ann's turn")
+      expect(page).not_to have_content("Simon's turn")
+    end
+  end
+
 end
