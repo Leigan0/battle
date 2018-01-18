eba40473fccb87802bc428e64c6d5ef2d85655e5
Leigh-ann <leighann.gant@gmail.com>
2018-01-18 22:00:30 +0000

    Class method created to create new instance of game

--- app.rb
+++ app.rb
@@ -13,38 +13,37 @@ class Battle < Sinatra::Base
   post '/names' do
     player_1 = Player.new(params[:player_1_name])
     player_2 = Player.new(params[:player_2_name])
-    $game = Game.new(player_1, player_2)
+    Game.create(player_1, player_2)
     redirect '/play'
   end
 
   post '/attack' do
-    @game = $game
-    Attack.new.attack(@game.player_under_attack)
+    Attack.new.attack(Game.instance.player_under_attack)
     redirect '/attack'
   end
 
   get '/attack' do
-    @game = $game
+    @game = Game.instance
     erb :attack
   end
 
   post '/switch_turn' do
-    @game = $game
+    @game = Game.instance
     @game.switch_turn
     redirect '/play'
   end
 
   get '/play' do
-    @game = $game
+    @game = Game.instance
     redirect '/gameover' if @game.gameover?
     erb :play
   end
 
   get '/gameover' do
-    @game = $game
+    @game = Game.instance
     erb :gameover
   end
-  
+
   run! if app_file == $0 # adding this allows you to run ruby app.rb within IRB
   #without this you would need to run rackup -p 4567
 end
