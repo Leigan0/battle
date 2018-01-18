96dd439a81cf2ebcaca2341484ad9be698017934
Leigh-ann <leighann.gant@gmail.com>
2018-01-18 13:57:13 +0000

    Attack responsibility removed from game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,13 +5,6 @@ subject(:game) { described_class.new(player_1, player_2) }
 let(:player_1) { double :player_1}
 let(:player_2) { double :player_2}
 
-  describe '#attack' do
-    it 'damages the player' do
-      expect(player_1).to receive(:attacked)
-      game.attack(player_1)
-    end
-  end
-
   describe '.initialize' do
     it 'accepts two arguments and stores players in array' do
       expect(game.players).to eq [player_1, player_2]
