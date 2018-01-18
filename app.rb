c86e9d436c7a4adea4f7b7dd40e0d3a8fa8b2f23
Leigh-ann <leighann.gant@gmail.com>
2018-01-18 20:32:38 +0000

    Gameover feature implemented

--- app.rb
+++ app.rb
@@ -36,9 +36,15 @@ class Battle < Sinatra::Base
 
   get '/play' do
     @game = $game
+    redirect '/gameover' if @game.gameover?
     erb :play
   end
 
+  get '/gameover' do
+    @game = $game
+    erb :gameover
+  end
+  
   run! if app_file == $0 # adding this allows you to run ruby app.rb within IRB
   #without this you would need to run rackup -p 4567
 end
